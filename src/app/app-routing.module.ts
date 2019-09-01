4370e5f21f078cea9a18f97162f17682b02fc790
Killerlive-cloud <setecsa307@gmail.com>
2019-09-01 16:31:52 -0500

    vista home

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'mesas', loadChildren: './mesas/mesas.module#MesasPageModule' },
+  { path: 'cocina', loadChildren: './cocina/cocina.module#CocinaPageModule' },
+  { path: 'caja', loadChildren: './caja/caja.module#CajaPageModule' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
